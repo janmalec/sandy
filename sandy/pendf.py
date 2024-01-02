1d8de3ebf4974d82f3a5906f030b471480cd18ce
Jan Malec <jan.malec@protonmail.com>
2024-01-02 18:28:49 +0100

    Read from text not file, processing

--- sandy/pendf.py
+++ sandy/pendf.py
@@ -6,6 +6,8 @@ from sandy.core.endf6 import _FormattedFile
 
 import logging
 import functools
+from tempfile import TemporaryDirectory
+from os.path import join
 
 __author__ = "Jan Malec"
 __all__ = [
@@ -16,25 +18,22 @@ __version__ = "0.1.0"
 
 pd.options.display.float_format = '{:.5e}'.format
 
-def split_pendf_by_temperature(file_path):
+def split_pendf_by_temperature(text):
     """
     Splits a PENDF file into sections based on temperature.
 
     Args:
-        file_path (str): The path to the PENDF file.
+        text (str): The content of the PENDF file.
 
     Returns:
         dict: A dictionary where the keys are temperature section IDs and the values are the corresponding sections as strings.
     """
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-
     temperature_sections = {}
     current_temp_id = 1
     last_index = 0
     last_mat_mf_mt = None
 
-    for line in lines:
+    for line in text:
         # Extract mat, mf, mt, and index values
         mat, mf, mt, index = int(line[66:70].strip()), int(line[70:72].strip()), int(line[72:75].strip()), int(line[75:80].strip())
 
@@ -66,18 +65,18 @@ def split_pendf_by_temperature(file_path):
 
     return temperature_sections
 
-def get_pendf_endf(file_path):
+def get_pendf_endf(text):
     """
     Returns a sandy.endf6 object from pendf files with
     multiple temperatures.
 
     Parameters:
-    file_path (str): The path to the pendf file.
+    text (str): The content of the PENDF file.
 
     Returns:
     dict: A dictionary containing sandy.endf6 objects for each temperature section.
     """
-    temperature_sections = split_pendf_by_temperature(file_path)
+    temperature_sections = split_pendf_by_temperature(text)
     pendf_sections = {temp_id: sandy.Endf6.from_text(section) for temp_id, section in temperature_sections.items()}
     return pendf_sections
 
@@ -123,6 +122,10 @@ def combine_pendf_sections(temperature_sections):
                 line = section_lines[counter]
                 counter += 1
 
+                # skip on empty line
+                if line.strip() == '':
+                    continue
+
                 mat, mf, mt = extract_mat_mf_mt(line)
 
                 if current_mat_mf_mt is None and mf != 0 and mt != 0:
@@ -150,4 +153,30 @@ def combine_pendf_sections(temperature_sections):
             if temp_id == max(preprocessed_sections.keys()):
                 current_mat_mf_mt = None
 
-    return '\n'.join(combined_lines)
+    return '\n'.join(combined_lines) + '\n'
+
+def make_pendfs(endf, **kwargs,):
+    """
+    Like get_pendf method from Endf6 class, but for pendf files with multiple temperatures.
+
+    Args:
+        endf (Endf6): The ENDF-6 object.
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        Union[Endf6, Tuple[Endf6, Endf6]]: The output ENDF-6 object(s).
+    """
+    kwargs["acer"] = False
+
+    with TemporaryDirectory() as td:
+        endf6file = join(td, "endf6_file")
+        endf.to_file(endf6file)
+        outputs = sandy.njoy.process_neutron(
+            endf6file,
+            suffixes=[0],
+            **kwargs,
+            )
+    if kwargs.get("dryrun", False):
+        return outputs  # this contains the NJOY input
+    # Split the output pendf and return endf objects
+    return get_pendf_endf(outputs["pendf"])
